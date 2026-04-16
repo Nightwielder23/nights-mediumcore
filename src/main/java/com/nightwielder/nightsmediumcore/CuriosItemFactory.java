68f982b02eec365b15f89f07a9ebd16cfca49d2d
Nightwielder23 <ayandeshpande@hotmail.com>
2026-04-16 10:28:33 -0700

    Added internal ModList guards to CuriosItemFactory methods as defensive programming

--- src/main/java/com/nightwielder/nightsmediumcore/CuriosItemFactory.java
+++ src/main/java/com/nightwielder/nightsmediumcore/CuriosItemFactory.java
@@ -2,6 +2,7 @@
 package com.nightwielder.nightsmediumcore;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.ModList;
 
 /**
  * Isolated factory to prevent HeartRelicItem (which implements ICurioItem)
@@ -12,16 +13,22 @@ public class CuriosItemFactory
 {
     public static Item createHeartRelic()
     {
+        if (!ModList.get().isLoaded("curios"))
+            return new Item(new Item.Properties().stacksTo(1));
         return new HeartRelicItem(new Item.Properties().stacksTo(1));
     }
 
     public static Item createBloodRelic()
     {
+        if (!ModList.get().isLoaded("curios"))
+            return new Item(new Item.Properties().stacksTo(1));
         return new BloodRelicItem(new Item.Properties().stacksTo(1));
     }
 
     public static boolean isEquippedInCurios(net.minecraft.world.entity.player.Player player, Item item)
     {
+        if (!ModList.get().isLoaded("curios"))
+            return false;
         return top.theillusivec4.curios.api.CuriosApi.getCuriosInventory(player)
                 .map(inv -> !inv.findCurios(item).isEmpty())
                 .orElse(false);
